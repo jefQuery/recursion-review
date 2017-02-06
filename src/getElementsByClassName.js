06c058586a92d3f1fe45adddc295315ee4e04414
jefQuery <jeffrey.milberger@gmail.com>
2017-02-06 12:32:40 -0800

    Add full functionality to getElementByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node = document.body) {
+  //document.body, element.childNodes, and element.classList
+
+  let results = [];
+  let classNameList = node.className.split(" ");
+  
+  //check if node has class === className, split class by (" "), check if _.some(class, className);
+  if (classNameList.includes(className)){
+    //if true, push to results array
+    results.push(node);
+  } 
+  
+  //check for children in node
+  if (node.children !== undefined && node.children.length > 0){
+    //var children = node.childNodes
+    for (let i = 0; i < node.children.length; i++){
+      //if children, iterate over children, recursively call getElementsByClassName
+      results = results.concat(getElementsByClassName(className, node.children[i]));
+    }
+  }
+
+  return results;
 };
