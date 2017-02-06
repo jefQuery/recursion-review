3f3a025f540db07033839b41196d20aa2dccdd33
jefQuery <jeffrey.milberger@gmail.com>
2017-02-06 12:08:20 -0800

    add full functionality to stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,29 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+  if(obj === null){
+    return 'null';
+
+  } else if (typeof obj !== 'object'){
+    if (typeof obj === 'string') {
+      return `"${obj}"`;
+    }
+    return `${obj}`;
+  } else if (Array.isArray(obj)) {
+    let results = obj.map(item => stringifyJSON(item));
+
+    return `[${results.join(",")}]`;
+  } else {
+    let results = [];
+    
+    for (let key in obj) {
+      if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+        results.push(`"${key}":${stringifyJSON(obj[key])}`);
+      }
+    }
+
+    return `{${results.join(',')}}`;
+  }
+
 };
