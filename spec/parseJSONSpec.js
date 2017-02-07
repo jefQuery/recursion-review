daa37c040a0ed902ceb4517eb71b8d7c772a55af
jefQuery <jeffrey.milberger@gmail.com>
2017-02-06 18:16:40 -0800

    Add base method structure to parseJSON, new number, bool tests

--- spec/parseJSONSpec.js
+++ spec/parseJSONSpec.js
@@ -1,27 +1,54 @@
 // test cases are described in fixtures.js
 describe('parseJSON', function() {
 
-  it('should match the result of calling JSON.parse', function() {
-    parseableStrings.forEach(function(test) {
-      var result = parseJSON(test);
-      var expected = JSON.parse(test);
-      var equality = _.isEqual(result, expected); // why can't we use `===` here?
-      // Replace this line with an `expect` statement that tests
-      // the behavior described by the `it` string
-      throw new Error('Test is missing.')
-    });
+  it('should return null, true and false', function() {
+    expect(parse.nullBool('null')).to.equal(null);
+    parse.index = 0;
+    expect(parse.nullBool('true')).to.equal(true);
+    parse.index = 0;
+    expect(parse.nullBool('false')).to.equal(false);
   });
 
-  it('should throw an error for invalid stringified JSON', function() {
-    unparseableStrings.forEach(function(test) {
-      var fn = function() {
-        parseJSON(test);
-      };
-      // if you'd prefer, you can write your version of parseJSON 
-      // so that it passes this test instead of the one on line 21. 
-      // expect(parseJSON(test)).to.equal(undefined);
-      expect(fn).to.throw(SyntaxError);
-    });
+  it('should parse basic numbers', function() {
+    expect(parse.numbers('9')).to.equal(9);
+    parse.index = 0;
+    expect(parse.numbers('-9')).to.equal(-9);
+    parse.index = 0;
+    expect(parse.numbers('-0.92')).to.equal(-0.92);
+    parse.index = 0;
+    expect(parse.numbers('9.65')).to.equal(9.65);
+    parse.index = 0;
+    expect(parse.numbers('9e6')).to.equal(9000000);
+    parse.index = 0;
+    expect(parse.numbers('9E6')).to.equal(9000000);
+    parse.index = 0;
+    expect(parse.numbers('9e-6')).to.equal(0.000009);
+    parse.index = 0;
+    expect(parse.numbers('9e+6')).to.equal(9000000);
   });
+//   it('should match the result of calling JSON.parse', function() {
+//     parseableStrings.forEach(function(test) {
+//       var result = parseJSON(test);
+//       var expected = JSON.parse(test);
+//       var equality = _.isEqual(result, expected); // why can't we use `===` here?
+      
+//       // Replace this line with an `expect` statement that tests
+//       expect(equality).to.equal(true);
+//       // the behavior described by the `it` string
+//       //throw new Error('Test is missing.');
+//     });
+//   });
+
+//   it('should throw an error for invalid stringified JSON', function() {
+//     unparseableStrings.forEach(function(test) {
+//       var fn = function() {
+//         parseJSON(test);
+//       };
+//       // if you'd prefer, you can write your version of parseJSON 
+//       // so that it passes this test instead of the one on line 21. 
+//       // expect(parseJSON(test)).to.equal(undefined);
+//       expect(fn).to.throw(SyntaxError);
+//     });
+//   });
 
 });
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,82 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  // iterate over the entire JSON string
+  let index = 0;
+  //trim whitsespace
+  return parse.JSON;
+};
+
+var parse = {
+  index: 0,
+
+  nullBool(json) {
+    if (json[this.index] === 'n') {
+      if (json.substr(this.index, 4) === 'null') {
+        this.index += 4;
+        return null;
+      }
+    } else if (json[this.index] === 't') {
+      if (json.substr(this.index, 4) === 'true') {
+        this.index += 4;
+        return true;
+      }
+    } else if (json[this.index] === 'f') {
+      if (json.substr(this.index, 5) === 'false') {
+        this.index += 5;
+        return false;
+      }
+    }
+  },
+
+  numbers(json) {
+    let resultsStr = '';
+    if (json[0] === '-') {
+      resultsStr += '-';
+      this.index++;
+    }
+    if (json[0] === '0') {
+      this.index++;
+      if (json[index] !== '.' || json[index].toLowerCase() !== 'e') {
+        throw new Error ('NaN!!!');
+      } 
+    }
+    this.index++;
+  },
+
+  strings(json) {
+
+  },
+  
+  arrays(json) {
+
+  },
+ 
+  objects(json) {
+
+  },
+
+  JSON(json) {
+
+    json = json.trim();
+
+    // check for what character we are on and what function we should call.
+    while (index !== undefined) {
+
+      // if (json[index] === ' ') index++;
+      // check for n and that goes into a null function if n is found
+      // if (json[index] === 'f' || json[index] === 't' || json[index] === 'n'){
+      this.nullBool(json);
+      
+      // if (json[index] === '"') call string
+      // make array of digits and negative sign to check for number
+      // if (numberTestArray.includes(json[index])) call number function
+      // if (json[index] === '[') call array function
+      // if (json[index] === '{') call object function
+
+
+      index++;//usually
+    }
+  }
+
 };
